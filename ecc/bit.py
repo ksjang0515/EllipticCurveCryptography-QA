baea6b20fb470654de740eb8c89dfd08a8d22e72
KyuSang Jang <ksjang0515@gmail.com>
2022-12-04 19:44:10 +0900

    removed changed variable

--- ecc/bit.py
+++ ecc/bit.py
@@ -4,7 +4,6 @@ from typing import Union
 class Bit:
     def __init__(self, index: Union[int, str]):
         self._index: str = index if isinstance(index, str) else str(index)
-        self.changed: bool = False
 
     @property
     def index(self) -> str:
@@ -13,7 +12,6 @@ class Bit:
     @index.setter
     def index(self, value) -> None:
         self._index = value
-        self.changed = True
 
     def __repr__(self):
         return self._index
