d2f5fa54eb3a8fc1dee25c8c279b1bb6115ff2d4
KyuSang Jang <ksjang0515@gmail.com>
2022-12-06 02:49:55 +0900

    changed Binary to include np.int8

--- ecc/types.py
+++ ecc/types.py
@@ -1,7 +1,8 @@
 from typing import Union
+import numpy as np
 
 Bit = int
-Binary = int
+Binary = Union[int, np.int8]  # dimod returns solver's result as np.int8
 Name = int
 Variable = list[Bit]
 Constant = Union[int, list[Binary]]
