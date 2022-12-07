5475f4e76a2ef0400b51ec0e7a05f47aa4e716ed
KyuSang Jang <ksjang0515@gmail.com>
2022-12-08 02:00:29 +0900

    fixed ecc_double to use int value

--- ecc/utilities/ecc_double.py
+++ ecc/utilities/ecc_double.py
@@ -2,10 +2,10 @@ from ecc.point import PointConst
 
 
 def ecc_double(A: PointConst, a: int, p) -> PointConst:
-    lambda_ = ((3*(A.x**2) + a) * pow(2*A.y, -1, p)) % p
+    lambda_ = ((3*(A.x_int**2) + a) * pow(2*A.y_int, -1, p)) % p
 
-    x = ((lambda_ ** 2) - (2*A.x)) % p
-    y = (lambda_ * (A.x - x) - A.y) % p
+    x = ((lambda_ ** 2) - (2*A.x_int)) % p
+    y = (lambda_ * (A.x_int - x) - A.y_int) % p
 
     C = PointConst(x, y)
     return C
