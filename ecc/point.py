07e276287ce7757dcdd19c3eb9c1d52e381201ae
KyuSang Jang <ksjang0515@gmail.com>
2022-12-07 13:10:33 +0900

    removed unnecessary properties

--- ecc/point.py
+++ ecc/point.py
@@ -1,4 +1,5 @@
 from ecc.types import Variable, Constant
+from ecc.utilities.number_to_binary import number_to_binary
 
 
 class Point:
@@ -7,31 +8,14 @@ class Point:
             raise ValueError('Length of x and y is different')
 
         self.length = len(x)
-        self._x = x
-        self._y = y
-
-    @property
-    def x(self) -> Variable:
-        return self._x
-
-    @property
-    def y(self) -> Variable:
-        return self._y
+        self.x = x
+        self.y = y
 
 
 class PointConst:
-    def __init__(self, x: Constant, y: Constant):
-        if len(x) != len(y):
-            raise ValueError('Length of x and y is different')
-
-        self.length = len(x)
-        self._x = x
-        self._y = y
-
-    @property
-    def x(self) -> Constant:
-        return self._x
-
-    @property
-    def y(self) -> Constant:
-        return self._y
+    def __init__(self, x: int, y: int, length: int = 256):
+        self.length = length
+        self.x: Constant = number_to_binary(x, length)
+        self.y: Constant = number_to_binary(y, length)
+        self.x_int = x
+        self.y_int = y
