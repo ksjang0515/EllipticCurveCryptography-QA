677d5c9bd2de461cf8bfc7387e8100db73ac8ce2
KyuSang Jang <ksjang0515@gmail.com>
2022-12-07 09:58:36 +0900

    import BitController

--- ecc/controller/__init__.py
+++ ecc/controller/__init__.py
@@ -1,4 +1,5 @@
 from .base_controller import BaseController
+from .bit_controller import BitController
 from .gate_controller import GateController
 from .arithmetic_controller import ArithmeticController
 from .modulo_controller import ModuloController
